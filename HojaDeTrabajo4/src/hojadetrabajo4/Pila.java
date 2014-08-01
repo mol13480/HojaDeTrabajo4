fca7dda0354466120297ba872a6e918219ff2993
Sami <Sami@HMaster>
2014-08-01 08:35:51 -0600

    Creacion de la interfaz pila

--- HojaDeTrabajo4/src/hojadetrabajo4/Pila.java
+++ HojaDeTrabajo4/src/hojadetrabajo4/Pila.java
@@ -8,8 +8,28 @@ package hojadetrabajo4;
 
 /**
  *
- * @author Irene
+ * @author Irene Molina
+ * @author Moises Urias
  */
-public interface Pila {
-    
+public interface Pila<E> {
+   public void push(E item);
+   // pre: 
+   // post: item is added to stack
+   // will be popped next if no intervening push
+   
+   public E pop();
+   // pre: stack is not empty
+   // post: most recently pushed item is removed and returned
+   
+   public E peek();
+   // pre: stack is not empty
+   // post: top value (next to be popped) is returned
+   
+   public boolean empty();
+   // post: returns true if and only if the stack is empty
+   
+   public int size();
+   // post: returns the number of elements in the stack
+   
+
 }
