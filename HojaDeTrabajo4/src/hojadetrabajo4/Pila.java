a4582a38117aa84221e0e8ea07d42fe630e4569e
Sami <Sami@HMaster>
2014-08-07 16:33:58 -0600

    Clase stack factory

--- HojaDeTrabajo4/src/hojadetrabajo4/Pila.java
+++ HojaDeTrabajo4/src/hojadetrabajo4/Pila.java
@@ -1,16 +1,10 @@
-/*
-Universidad del Valle de Guatemala
-Hoja de Trabajo 3 
-Irene Molina 13480
-Jorge Garcia 13175
-Moises Urias 13015
- */
+
 
 
 package hojadetrabajo4;
 
 /**
- *
+ * Esta interface contiene todos los metodos que son necesarios al momento de
  * @author Irene Molina
  * @author Moises Urias
  */
--- HojaDeTrabajo4/src/hojadetrabajo4/StackFactory.java
+++ HojaDeTrabajo4/src/hojadetrabajo4/StackFactory.java
@@ -2,7 +2,6 @@
 Universidad del Valle de Guatemala
 Hoja de Trabajo 3 
 Irene Molina 13480
-Jorge Garcia 13175
 Moises Urias 13015
  */
 
@@ -10,9 +9,61 @@ Moises Urias 13015
 package hojadetrabajo4;
 
 /**
- *
- * @author Irene
+ * Esta clase selecciona la implementacino que se va a utilizar para una pila,
+ * dependiendo de lo que seleccino el usuario. Esta clase utiliza el pratron
+ * Factory
+ * 
+ * @author Irene Molina
+ * @author Moises Urias
  */
-public class StackFactory {
+public class StackFactory<E> {
+    
+    
+    /**
+     * Selecciona la implementacion a utilizar dependiendo del parametro que se
+     * ingrese
+     * 
+     * @param entry es un numero con el patro que se desea utilizar
+     * @return implementacion. Es la implementacion que se va utilizar
+     */
+    public Pila<E> getStack(int entry)
+    {
+        /*La implementacion que se va a devolver*/
+        Pila<String> implementacion = null;
+        
+        /*Si se selecciona el numero 1, entonces se devuelve la implementacion
+        * StackArrayList*/
+        if (entry == 1)
+        {
+            implementacion =  StackArrayList.stackarraylist;
+        }
+        /*Si se selecciona el numero 2, entonces se devuelve la implementacion
+        * StackVector*/
+        else if(entry == 2)
+        {
+            implementacion = new StackVector<E>();
+        }
+        /*Si se selecciona el numero 3, entonces se devuelve la implementacion
+        * Encadenada simple*/
+        else if(entry == 3)
+        {
+            implementacion = new EncadenadaSimple<E>();
+        }
+        /*Si se selecciona el numero 3, entonces se devuelve la implementacion
+        * Encadenada Doble*/
+        else if(entry == 4)
+        {
+            implementacion = new EncadenadaDoble<E>();
+        }
+        /*Si se selecciona el numero 3, entonces se devuelve la implementacion
+        * Circular*/
+        else if(entry == 5)
+        {
+            implementacion = new Circular<E>();
+        }
+        
+        /*Se devuelve la implementacion deseada*/
+        return implementacion;
+    }
     
 }
